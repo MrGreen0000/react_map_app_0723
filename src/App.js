26654febf7faffad3b5cb9b5bb8e4cfddc265c68
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-07-11 15:17:33 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import * as React from "react";
+import Map, { NavigationControl } from "react-map-gl";
+import Navbar from "./components/navbar.js";
+import maplibregl from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Map
+        mapLib={maplibregl}
+        initialViewState={{
+          longitude: 16.62662018,
+          latitude: 49.2125578,
+          zoom: 14,
+        }}
+        style={{ width: "100%", height: " calc(100vh - 77px)" }}
+        mapStyle="https://api.maptiler.com/maps/satellite/style.json?key=bUNkGxzojTZOBuCGooQ2"
+      >
+        <NavigationControl position="top-left" />
+      </Map>
+    </div>
+  );
+}
+
+export default App;
